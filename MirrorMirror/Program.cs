b59ac674eebe75536893fdb554075b1c9340fc05
Your Name <you@example.com>
2022-12-21 23:32:06 +0100

    Rough PoC

--- MirrorMirror/Program.cs
+++ MirrorMirror/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MirrorMirror
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MirrorMirror
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
+			StartWebServer("http://localhost:5000");
+			Console.ReadLine();
+		}
+
+		public static void StartWebServer(string url)
+		{
+			var host = WebHost.CreateDefaultBuilder()
+				.ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
+				.UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True")
+				.UseStartup<ServerStartup>()
+				.UseUrls(url)
+				.Build();
+
+			host.Run();
 		}
 	}
 }
---
+++ MirrorMirror/ServerStartup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MirrorMirror
+{
+	internal class ServerStartup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			//app.UseRouting();
+			app.Use(async (context, next) =>
+			{
+				var request = context.Request;
+
+				// Parse Body
+				request.EnableBuffering();
+				if (request.Body.CanSeek)
+				{
+					request.Body.Position = 0;
+				}
+				var rawRequestBody = new StreamReader(context.Request.Body).ReadToEnd();
+
+
+				// Parse Headers
+				var processedHeaders = new List<string>();
+				foreach(var header in request.Headers)
+				{
+					processedHeaders.Add($"{header.Key}: {header.Value}");
+				}
+
+				var rawHTTP =
+				$"{request.Method} {request.Scheme}://{request.Host}{request.Path} {request.Protocol}" + Environment.NewLine +
+				$"{string.Join(Environment.NewLine, processedHeaders)}" + Environment.NewLine + ""
+				+ Environment.NewLine +
+				$"{rawRequestBody}";
+				
+				Console.Write(rawHTTP);
+				await next();
+			});
+
+			//app.UseEndpoint();
+		}
+	}
+}
