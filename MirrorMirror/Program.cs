1ecc83020178a0c7196993d2628442938b041e50
Your Name <you@example.com>
2022-12-27 08:27:11 +0100

    Cobbled together mvp

--- MirrorMirror/Program.cs
+++ MirrorMirror/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Spectre.Console;
 
 namespace MirrorMirror
 {
@@ -8,21 +9,116 @@ namespace MirrorMirror
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-			StartWebServer("http://localhost:5000");
+			var arguments = args.ToList();
+			var bindUrls = new List<string>();
+			var ports = new List<int>() { 8080 };
+			var ip = "127.0.0.1";
+
+			ShowStartupBanner();
+
+			// Check if help banner should be shown
+			if (arguments.Count == 0 || arguments.Any(x => x.Contains("-h") || x.Contains("--help") || x.Contains("-help")))
+			{
+				PrintHelp();
+				return;
+			}
+
+			// Parse IP
+			if (arguments.Any(x => x.Contains("-i")) && arguments.Count >= 2)
+			{
+				var ipIndex = arguments.IndexOf("-i") + 1;
+				ip = arguments[ipIndex];
+			}
+
+			// Parse Ports
+			if (arguments.Any(x => x.Contains("-p")) && arguments.Count >= 2)
+			{
+				var portIndex = arguments.IndexOf("-p") + 1;
+				ports.Clear();
+				
+				for (; portIndex < arguments.Count; portIndex++)
+				{
+					var parsedPort = 0;
+					var success = Int32.TryParse(arguments[portIndex], out parsedPort);
+
+					if(!success || parsedPort == 0) {
+						continue;
+					}
+
+					ports.Add(parsedPort);
+				}
+			}
+
+			// Setup URLs to bind to
+			
+			foreach (var port in ports)
+			{
+				bindUrls.Add($"http://{ip}:{port}");
+			}
+
+			StartWebServer(bindUrls);
 			Console.ReadLine();
 		}
 
-		public static void StartWebServer(string url)
+		private static void PrintHelp()
+		{
+			AnsiConsole.MarkupLine(@"
+[royalblue1]-h[/]               Print this help.
+[royalblue1]-p[/]               Comma seperated list of ports to listen on. Default is 8080. (i.e. -p 1337,8080,1234)
+[royalblue1]-i[/]               IP to bind to. Default is 127.0.0.1. (i.e. -i 192.168.1.123)");
+		}
+
+		private static void StartWebServer(IList<string> bindUrls)
 		{
 			var host = WebHost.CreateDefaultBuilder()
 				.ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
 				.UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True")
 				.UseStartup<ServerStartup>()
-				.UseUrls(url)
+				.UseUrls(bindUrls.ToArray())
 				.Build();
 
-			host.Run();
+			AnsiConsole.Status()
+			.AutoRefresh(true)
+			.Spinner(Spinner.Known.Aesthetic)
+			.Start("[green]Waiting for requests..[/]", ctx =>
+			{
+				AnsiConsole.MarkupLine($"Web Server bound to:");
+
+				foreach (var url in bindUrls)
+				{
+					AnsiConsole.MarkupLine($"[royalblue1]{url}[/]");
+				}
+
+				AnsiConsole.MarkupLine("");
+
+				try {
+					host.Run();
+				} catch(Exception e) {
+					AnsiConsole.MarkupLine("[red](!) Exception while starting Web Server:[/]");
+					AnsiConsole.MarkupLine(e.Message);
+					AnsiConsole.MarkupLine("");
+				}
+			});
+
+			// Done
+			AnsiConsole.MarkupLine("[red](!) Web Server terminated[/]");
+
+		}
+
+		private static void ShowStartupBanner()
+		{
+
+			AnsiConsole.MarkupLine(@"[royalblue1]
+ __  __ _                     __  __ _                     
+|  \/  (_)__ _ __ _  ___ __ _|  \/  (_)__ _ __ _  ___ __ _ 
+| |\/| | |__` |__` |/ _ |__` | |\/| | |__` |__` |/ _ |__` |
+| |  | | |  | |  | | (_) | | | |  | | |  | |  | | (_) | | |
+|_|  |_|_|  |_|  |_|\___/  |_|_|  |_|_|  |_|  |_|\___/  |_|
+                                                           
+[/]");
+			var rule = new Rule();
+			rule.RuleStyle("red dim");
+			AnsiConsole.Write(rule);
 		}
 	}
 }
--- MirrorMirror/ServerStartup.cs
+++ MirrorMirror/ServerStartup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Spectre.Console;
 
 namespace MirrorMirror
 {
@@ -10,6 +12,12 @@ namespace MirrorMirror
 			//app.UseRouting();
 			app.Use(async (context, next) =>
 			{
+				if(RequestCount == 0)
+				{
+					Console.Clear();
+				}
+
+				RequestCount++;
 				var request = context.Request;
 
 				// Parse Body
@@ -25,20 +33,34 @@ namespace MirrorMirror
 				var processedHeaders = new List<string>();
 				foreach(var header in request.Headers)
 				{
-					processedHeaders.Add($"{header.Key}: {header.Value}");
+					processedHeaders.Add($"[grey74]{header.Key}:[/] {header.Value}");
 				}
 
-				var rawHTTP =
-				$"{request.Method} {request.Scheme}://{request.Host}{request.Path} {request.Protocol}" + Environment.NewLine +
-				$"{string.Join(Environment.NewLine, processedHeaders)}" + Environment.NewLine + ""
-				+ Environment.NewLine +
-				$"{rawRequestBody}";
+				PrintTableHeader(context.Connection.RemoteIpAddress.ToString(), context.Connection.RemotePort, request.Headers.Count);
+
+				AnsiConsole.Markup($"[indianred1]{request.Method}[/] {request.GetDisplayUrl()} {request.Protocol}" + Environment.NewLine +
+								   $"{string.Join(Environment.NewLine, processedHeaders)}" + Environment.NewLine + ""
+								   + Environment.NewLine +
+								   $"{rawRequestBody}");
 				
-				Console.Write(rawHTTP);
 				await next();
 			});
-
-			//app.UseEndpoint();
 		}
+
+		private void PrintTableHeader(string originIP, int originPort, int headerCount)
+		{
+			var table = new Table();
+			table.Border = TableBorder.Rounded;
+			table.BorderColor(Color.Violet);
+			table.Expand();
+			table.AddColumn($"# {RequestCount}");
+			table.AddColumn($"Received: {DateTime.Now}");
+			table.AddColumn($"Origin IP: {originIP}");
+			table.AddColumn($"Origin Port: {originPort}");
+			table.AddColumn($"Headers: {headerCount}");
+			AnsiConsole.Write(table);
+		}
+
+		private static int RequestCount = 0;
 	}
 }
